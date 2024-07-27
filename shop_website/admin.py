1681d070244cd74732bb3465b2d2f135d8ca8f12
Daisy <shahzodaakhmedova91@gmail.com>
2024-07-27 22:42:55 +0300

    2nd commit

--- shop_website/admin.py
+++ shop_website/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, User, Comment, Order
 
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Order)
